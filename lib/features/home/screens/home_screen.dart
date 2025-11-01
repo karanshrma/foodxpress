74739ca8e60f90d194da55ace6155c171590a0eb
karanshrma <ks2275863@example.com>
2025-11-02 00:56:34 +0530

    formatted home screen

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,5 +1,69 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+const List<Map<String, dynamic>> foodItems = [
+  {
+    'name': 'Margherita Pizza',
+    'image':
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQZT-mMFzbCHQ1IC81oKT1oNlK445qt_JNgQ&s',
+    'price': 249,
+    'description':
+    'Classic pizza topped with mozzarella, tomato sauce, and basil leaves.',
+  },
+  {
+    'name': 'Cheese Burger',
+    'image':
+    'https://images.unsplash.com/photo-1572802419224-296b0aeee0d9?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hlZXNlYnVyZ2VyfGVufDB8fDB8fHww&fm=jpg&q=60&w=3000',
+    'price': 199,
+    'description':
+    'Juicy grilled beef patty layered with cheese, lettuce, and special sauce.',
+  },
+  {
+    'name': 'Chicken Biryani',
+    'image':
+    'https://www.shutterstock.com/image-photo/traditional-chicken-biryani-served-brass-600nw-2622739739.jpg',
+    'price': 299,
+    'description':
+    'Aromatic basmati rice cooked with tender chicken and Indian spices.',
+  },
+  {
+    'name': 'Cold Coffee',
+    'image':
+    'https://cdn.pixabay.com/photo/2021/01/06/10/11/coffee-5893926_1280.jpg',
+    'price': 149,
+    'description':
+    'Refreshing blend of coffee, milk, and ice for a perfect chill treat.',
+  },
+  {
+    'name': 'Veg Sandwich',
+    'image':
+    'https://img.freepik.com/free-photo/close-up-boiled-egg-tomatoes-sandwich_23-2148590168.jpg?semt=ais_hybrid&w=740&q=80',
+    'price': 129,
+    'description':
+    'Loaded with fresh veggies, cheese, and sauces between toasted bread.',
+  },
+  {
+    'name': 'Chocolate Dessert',
+    'image':
+    'https://img.freepik.com/free-photo/closeup-freshly-baked-delicious-pumpkin-chocolate-brownie-with-ice-cream-plate_181624-38571.jpg?semt=ais_hybrid&w=740&q=80',
+    'price': 179,
+    'description':
+    'Rich chocolate brownie served warm with vanilla ice cream.',
+  },
+  {
+    'name': 'Pasta Alfredo',
+    'image':
+    'https://www.shutterstock.com/image-photo/chicken-alfredo-penne-creamy-pasta-600nw-2493019471.jpg',
+    'price': 229,
+    'description':
+    'Creamy white sauce pasta with chicken, herbs, and parmesan cheese.',
+  },
+];
+const List<Map<String, String>> categoryImages = [
+  {'title': 'Pizza', 'image': 'assets/images/pizza.jpg'},
+  {'title': 'Burger', 'image': 'assets/images/burger.jpg'},
+  {'title': 'Drinks', 'image': 'assets/images/drinks.jpg'},
+  {'title': 'Dessert', 'image': 'assets/images/dessert.jpg'},
+];
 
 class HomeScreen extends StatelessWidget {
   static route() => MaterialPageRoute(builder: (context) => HomeScreen());
@@ -86,12 +150,7 @@ class HomeScreen extends StatelessWidget {
   }
 
   Widget categorySection() {
-    const List<Map<String, String>> categoryImages = [
-      {'title': 'Pizza', 'image': 'assets/images/pizza.jpg'},
-      {'title': 'Burger', 'image': 'assets/images/burger.jpg'},
-      {'title': 'Drinks', 'image': 'assets/images/drinks.jpg'},
-      {'title': 'Dessert', 'image': 'assets/images/dessert.jpg'},
-    ];
+
 
     return Container(
       height: 150,
@@ -133,65 +192,6 @@ class HomeScreen extends StatelessWidget {
   }
 
   Widget foodList() {
-    const List<Map<String, dynamic>> foodItems = [
-      {
-        'name': 'Margherita Pizza',
-        'image':
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQZT-mMFzbCHQ1IC81oKT1oNlK445qt_JNgQ&s',
-        'price': 249,
-        'description':
-            'Classic pizza topped with mozzarella, tomato sauce, and basil leaves.',
-      },
-      {
-        'name': 'Cheese Burger',
-        'image':
-            'https://images.unsplash.com/photo-1572802419224-296b0aeee0d9?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hlZXNlYnVyZ2VyfGVufDB8fDB8fHww&fm=jpg&q=60&w=3000',
-        'price': 199,
-        'description':
-            'Juicy grilled beef patty layered with cheese, lettuce, and special sauce.',
-      },
-      {
-        'name': 'Chicken Biryani',
-        'image':
-            'https://www.shutterstock.com/image-photo/traditional-chicken-biryani-served-brass-600nw-2622739739.jpg',
-        'price': 299,
-        'description':
-            'Aromatic basmati rice cooked with tender chicken and Indian spices.',
-      },
-      {
-        'name': 'Cold Coffee',
-        'image':
-            'https://cdn.pixabay.com/photo/2021/01/06/10/11/coffee-5893926_1280.jpg',
-        'price': 149,
-        'description':
-            'Refreshing blend of coffee, milk, and ice for a perfect chill treat.',
-      },
-      {
-        'name': 'Veg Sandwich',
-        'image':
-            'https://img.freepik.com/free-photo/close-up-boiled-egg-tomatoes-sandwich_23-2148590168.jpg?semt=ais_hybrid&w=740&q=80',
-        'price': 129,
-        'description':
-            'Loaded with fresh veggies, cheese, and sauces between toasted bread.',
-      },
-      {
-        'name': 'Chocolate Dessert',
-        'image':
-            'https://img.freepik.com/free-photo/closeup-freshly-baked-delicious-pumpkin-chocolate-brownie-with-ice-cream-plate_181624-38571.jpg?semt=ais_hybrid&w=740&q=80',
-        'price': 179,
-        'description':
-            'Rich chocolate brownie served warm with vanilla ice cream.',
-      },
-      {
-        'name': 'Pasta Alfredo',
-        'image':
-            'https://www.shutterstock.com/image-photo/chicken-alfredo-penne-creamy-pasta-600nw-2493019471.jpg',
-        'price': 229,
-        'description':
-            'Creamy white sauce pasta with chicken, herbs, and parmesan cheese.',
-      },
-    ];
-
 
     return Padding(
       padding: const EdgeInsets.all(8.0),
